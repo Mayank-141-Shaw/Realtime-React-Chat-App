86b4e3b5ea06513170daffcee7aeadf4379ab7dc
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-07-24 23:48:17 +0530

    Created the register form structure

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 function Register() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Form ");
+  }
+
+  const handleChange = (e) => {
+    
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <FormContainer>
+        <form onSubmit={ (e)=>handleSubmit(ev) }>
+          <div className="brand">
+            <img src="" alt="" />
+            <h1>snappy</h1>
+          </div>
+          <input 
+            type="text" 
+            placeholder='Username' 
+            name='username' 
+            onChange={e=>handleChange(e)}
+          />
+          <input 
+            type="email" 
+            placeholder='Email' 
+            name='email' 
+            onChange={e=>handleChange(e)}
+          />
+          <input 
+            type="password" 
+            placeholder='Password' 
+            name='password' 
+            onChange={e=>handleChange(e)}
+          />
+          <input 
+            type="password" 
+            placeholder='Confirm Password' 
+            name='confirmPassword' 
+            onChange={e=>handleChange(e)}
+          />
+          <button type='submit'>Create User</button>
+          <span>
+            Already have an account ? <Link to='/login'>Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+    </>
   )
 }
+
+const FormContainer = styled.div``;
 
 export default Register
