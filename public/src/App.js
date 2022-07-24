c0d0395c34ff6ff549baedee857295d54268cd22
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-07-24 23:21:40 +0530

    Added routes

--- public/src/App.js
+++ public/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './components/Register'
+import Login from './components/Login'
+import Chat from './components/Chat'
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={ <Register /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/' element={ <Chat /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
