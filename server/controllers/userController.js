4b147bf21d742476bced6b7e49a9b9eb832a25a2
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-07-31 00:30:44 +0530

    Updated register contr with validation and save

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,46 @@
+
+// controllers for user endpoints, just like java spring RestController
+
+
+const User = require("../model/userModel");
+const bcrypt = require("bcrypt");
+
+
+// handles the post request for register
+// whenever user submits register button it is rerouted to this controller
+// this will then get the request and do required tasks
 module.exports.register = ( req, res, next ) => {
-    console.log(req.body);
+    
+    try{
+        const { username, email, password } = req.body;
+
+        // check if user is present inn database
+        const userNameCheck = await User.findOne({ username });
+        if(userNameCheck) 
+            return res.json({ msg:"Username already used", status: false });
+
+        // checck if email is present
+        const emailCheck = await User.findOne({ email });
+        if(emailCheck) 
+            return res.json({ msg:"User email already used", status: false });
+
+
+        // if everything is good
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            email,
+            username,
+            password: hashedPassword,
+        });
+
+        // delete original password after saving to db
+        delete user.password;
+
+        // return the response
+        return res.json({ status: true, user });
+        
+    } catch( err ){
+        next(err)
+    }
+
 };
---
+++ server/model/userModel.js
@@ -0,0 +1,34 @@
+
+const mongoose = require("mongoose");
+
+// schema for user model / entity
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50,
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 50,
+    },
+    isAvatarImageSet : {
+        type: Boolean,
+        default: false,
+    },
+    avatarImage : {
+        type: String,
+        default: "",
+    },
+})
+
+module.exports = mongoose.model("Users", userSchema);
