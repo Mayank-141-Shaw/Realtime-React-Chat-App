08234c0e69c9889eed53064593cdf663e3fb2157
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-08-02 16:55:51 +0530

    Fetching images into buffer array

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -4,6 +4,8 @@ import loader from '../assets/loader.gif'
 import styled from 'styled-components';
 import axios from 'axios';
 
+import { Buffer } from 'buffer'
+
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { setAvatarRoute } from '../utils/APIRoutes';
@@ -14,6 +16,33 @@ export default function SetAvatar() {
     const navigate = useNavigate();
 
     const [avatars, setAvatars] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectedAvatar, setSelectedAvatar] = useState(undefined);
+
+    // toast options
+    const toastOptions = {
+        position: 'bottom-right',
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: 'dark'
+    }
+    
+    const setProfilePicture = async () => {}
+
+    useEffect( ()=>{
+        const data = [];
+        for(let i=0; i<4; i++){
+            // gets a different random image out of 1 - 1000 images id
+            // we need 4 of them so looping 4 times
+            const image = await axios.get(`${api}/${Math.round(Math.random() * 1000)}`);
+
+            const buffer = new Buffer(image.data);
+            data.push(buffer.toString("base64"));
+        }
+        setAvatars(data)
+        setIsLoading(false)
+    }, [])
 
     return (
         <>
