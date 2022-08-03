5081e1a64cd519c93b7bfd526f45fdd0291f3b42
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-08-03 16:59:34 +0530

    added login check in home page

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function Chat() {
+
+  const navigate = useNavigate();
+
+  const [contacts, setContacts] = useState([])
+  const [curUser, setCurUser] = useState(undefined)
+
+  // to get logged user info
+  useEffect( () => {
+    // if not logged in first login
+    if(!localStorage.getItem('chat-app-user')){
+        navigate("/login");
+    }
+  })
+
+  // to get list of all contacts
+  useEffect( async () => {
+
+  }, [] )
+
   return (
     <Container>
       <div className="container">
