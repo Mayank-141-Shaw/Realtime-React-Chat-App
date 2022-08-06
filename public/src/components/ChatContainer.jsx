e05e5af1cea99c9cbcceb3e9660eaea6ffd89c8b
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-08-06 17:33:18 +0530

    Styled chat container

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Logout from './Logout';
 
 export default function ChatContainer({ currentChat }) {
   return (
@@ -20,6 +21,8 @@ export default function ChatContainer({ currentChat }) {
                   <h3>{currentChat.username}</h3>
                 </div>
               </div>
+
+              <Logout />
             </div>
       
             <div className="chat-messages"></div>
@@ -35,4 +38,30 @@ export default function ChatContainer({ currentChat }) {
 
 const Container = styled.div`
 
+  padding-top: 1rem;
+  
+  .chat-header{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 2rem;
+
+    .user-details{
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+
+      .avatar{
+        img{
+          height: 3rem;
+        }
+      }
+
+      .username{
+        h3{
+          color: white;
+        }
+      }
+    }
+  }
 `;
