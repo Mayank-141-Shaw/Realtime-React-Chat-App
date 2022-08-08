8afd1e49ffcd7b82ab4862878399954bb7e6e841
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-08-08 20:37:35 +0530

    Chat Input structure

--- public/src/components/ChatInput.jsx
+++ public/src/components/ChatInput.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+import Picker from 'emoji-picker-react'
+import { IoMdSend } from  'react-icons/io'
+import { BsEmojiSmileFill } from 'react-icons/bs'
 
 export default function ChatInput() {
   return (
-    <div>ChatInput</div>
+    <Container>
+        <div className="button-container">
+            <div className="emoji">
+                <BsEmojiSmileFill />
+            </div>
+        </div>
+
+        <form className='input-container'>
+            <input type="text" placeholder='type your message here' />
+            <button className='submit'>
+                <IoMdSend />
+            </button>
+        </form>
+    </Container>
   )
 }
+
+const Container = styled.div`
+
+`;
